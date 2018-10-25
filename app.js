96098d00b382df6e45d8fffe618991bdc4cd9dd8
shincap8 <43832784+shincap8@users.noreply.github.com>
2018-10-25 17:58:32 -0500

    Almost!
    Falta limitar el numero de digitos del resultado a 8 y permitir la secuencia de operaciones

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var Calculadora= {
-    display : document.getElementById('display'),
-    operadorA : "",
-    operadorB : "",
-   
-    //--------------------Mostrar Numeros en Pantalla----------------------
+    //---------------Iniciador de todas las funciones---------
+    init: function () {
+        this.cambiarbotones('tecla');
+        this.pantalla('tecla');
+    },
+    //--------------------------------------------------------
+   display : document.getElementById('display'),
+   operadorA : "",
+   operadorB : "",
+   operacion : "",
+
+    //--------------------Mostrar Numeros y hacer operaciones en Pantalla----------------------
     pantalla: function (selector) {
         var botonesPagina = document.getElementsByClassName(selector);
         for (i = 0; i < botonesPagina.length; i++) {
@@ -12,22 +19,85 @@ var Calculadora= {
     },
 
     eventocaptura: function (event) {
-        mostrar(event.target)
-        function mostrar(elemento) {
+        calcular(event.target)
+        function calcular(elemento) {
             boton = elemento.id;
             oprimido = parseInt(boton);
             if ((this.display.textContent == "0" || this.display.textContent == null) && !(isNaN(oprimido))){
                 this.display.textContent = boton;
             } else if (this.display.textContent.length < 8 && !(isNaN(oprimido))) {
                 this.display.textContent = this.display.textContent + boton;
+                 //---------------borrar numeros de la pantalla-----------------------
+            } else if (isNaN(oprimido) && boton == "on") {
+                ONC();
+                //------------------Solo es permitido un punto------------------------
+            } else if (boton == "punto" && this.display.textContent.length < 8 && this.display.textContent.indexOf(".") < 0) {
+                this.display.textContent = this.display.textContent + ".";
+                //-----------------Signo +/- -----------------------------------------
+            } else if (boton == "sign" && this.display.textContent.length <= 8 && this.display.textContent != "0" && this.display.textContent.indexOf("-") < 0) {
+                this.display.textContent = "-" + this.display.textContent;
+            } else if (boton == "sign" && this.display.textContent != "0" && this.display.textContent.indexOf("-") >= 0) {
+                nuevodisplay = this.display.textContent.replace("-","");
+                this.display.textContent = nuevodisplay;
+                //------------------------Suma--------------------------------------
+            } else if (boton == "mas") {
+                this.operadorA = this.display.textContent;
+                this.display.textContent = "";
+                operacion = "+"
+                //----------------------Resta--------------------------------------
+            } else if (boton == "menos") {
+                this.operadorA = this.display.textContent;
+                this.display.textContent = "";
+                operacion = "-"
+                //---------------------multiplicacion-----------------------------
+            } else if (boton == "por") {
+                this.operadorA = this.display.textContent;
+                this.display.textContent = "";
+                operacion = "*"
+                //--------------------División-------------------------------------
+            } else if (boton == "dividido") {
+                this.operadorA = this.display.textContent;
+                this.display.textContent = "";
+                operacion = "/"
+                //-------------------Igual-------------------------------------
+            } else if (boton == "igual") {
+                this.operadorB = this.display.textContent;
+                calculo();
             } else {
                 this.display.textContent = this.display.textContent;
             }
         }
+    //------------------------Función para borrar ---------------------------
+        function ONC() {
+            this.display.textContent = "0";
+            this.operadorA = 0;
+            this.operadorB = 0;
+            this.operacion = "";
+        }
+    //----------------------Función para calculo matematico----------------
+        function calculo() {
+            var cal = 0;
+            switch (operacion) {
+                case "+":
+                    cal = parseFloat(operadorA) + parseFloat(operadorB)
+                    break;
+                case "-":
+                    cal = parseFloat(operadorA) - parseFloat(operadorB)
+                    break;
+                case "*":
+                    cal = parseFloat(operadorA) * parseFloat(operadorB)
+                    break;
+                case "/":
+                    cal = parseFloat(operadorA) / parseFloat(operadorB)
+                    break;
+            }
+            this.display.textContent = ""
+            this.display.textContent = cal;
+        }
     },
     //------------------------------------------------------
     //--------cambiar tamaño a botones----------------------
-    cambiarbotones: function (selector) {
+    cambiarbotones: function(selector) {
         var botonesPagina = document.getElementsByClassName(selector);
         for (var i = 0; i < botonesPagina.length; i++) {
             botonesPagina[i].onmousedown = this.eventotamaño;
@@ -37,7 +107,7 @@ var Calculadora= {
     eventotamaño: function (event) {
         reducir(event.target)
         function reducir(elemento) {
-            elemento.style.padding = "1%";
+            elemento.style.padding = "1.3%";
             console.log(elemento.id);
         }
     },
@@ -48,14 +118,8 @@ var Calculadora= {
         }
     },
     //--------------------------------------------------------
-    //---------------Iniciador de todas las funciones---------
-    init: function () {
-        this.cambiarbotones('tecla');
-        this.pantalla('tecla');
-        this.ONC('tecla');
-    }
-    //--------------------------------------------------------
 }
 
-Calculadora.init();
+Calculadora.init()
+
 
